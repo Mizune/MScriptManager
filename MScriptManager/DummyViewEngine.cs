5a9649a3e8d418f559b1877d579fcb39d692ca36
Mizune <willful.slight@gmail.com>
2016-08-04 01:15:02 +0900

    create dummy engine.

---
+++ MScriptManager/DummyViewEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MScriptManager
+{
+    public class DummyViewEngine
+    {
+        // This is dummy sources . Only use test program.
+        public void ChangeBGI()
+        {
+            Console.WriteLine("Background image was changed.");
+        }
+
+
+    }
+}
