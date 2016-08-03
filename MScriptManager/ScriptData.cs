e0215850fb26ac5468c447d6552b5ad26415a970
Mizune <willful.slight@gmail.com>
2016-08-04 00:19:08 +0900

    Create frame and ScriptData

---
+++ MScriptManager/ScriptData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MScriptManager
+{
+    public class ScriptData
+    {
+        private int TypeNum { get ;set; }
+        private string Line { get; set; }
+
+        private List<string> Option;
+
+        public ScriptData()
+        {
+            Option = new List<string>();
+        }
+
+        public ScriptData(List<string> options)
+        {
+            foreach(var opt in options)
+            {
+                Option.Add(opt);
+            }
+        }
+
+        
+        public void AddOption(string option)
+        {
+            Option.Add(option);
+        }
+
+    }
+}
---
+++ MScriptManager/ScriptManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MScriptManager
+{
+    public class ScriptManager
+    {
+        private IEnumerable<ScriptData> data;
+        private Parser parser;
+        private Reader reader;
+
+    }
+}
